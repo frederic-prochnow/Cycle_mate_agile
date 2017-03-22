b1fd99d37021432c86aa96240ad7975416471f36
Frederic PROCHNOW <frederic.prochnow@etudiant.univ-lille1.fr>
2017-03-22 10:07:41 +0100

    2203171007 app android

---
+++ App_velo/app/src/main/java/prochnof/iutinfo/univlille1/fr/app_velo/MainActivity.java
@@ -0,0 +1,13 @@
+package prochnof.iutinfo.univlille1.fr.app_velo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
